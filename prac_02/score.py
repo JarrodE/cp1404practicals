1e487af0c4fbcfcf2c7768f8e0d139ab258d659e
JarrodE <jarrod.eaton@my.jcu.edu.au>
2023-09-24 23:05:43 +1000

    Refactor score check program to randomise and use functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,14 +3,31 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-score = float(input("Enter score: "))
-while score < 0 or score > 100:
-    print("Invalid score")
-    score = float(input("Enter score: "))
+import random
 
-if score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Pass")
-else:
-    print("Fail")
+
+def main():
+    # score = float(input("Enter score: "))
+    # score = parameters(score)
+    random_score = random.randint(0, 100)
+    print(f"Random score: {random_score}")
+    score_ranks(random_score)
+
+
+def score_ranks(score):
+    if score >= 90:
+        print("Excellent")
+    elif score >= 50:
+        print("Pass")
+    else:
+        print("Fail")
+
+
+def parameters(score):
+    while score < 0 or score > 100:
+        print("Invalid score")
+        score = float(input("Enter score: "))
+    return score
+
+
+main()
