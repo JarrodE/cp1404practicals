2e5217b9f631352a11837df8a8a4ce8337dec26a
JarrodE <jarrod.eaton@my.jcu.edu.au>
2023-09-24 23:33:56 +1000

    Add score menu program with functions

---
+++ prac_02/score_menu.py
@@ -0,0 +1,48 @@
+
+MENU = """(G)et score (must be 0-100)
+(P)rint result
+(S)how stars
+(Q)uit"""
+
+
+def main():
+    score = float(input("Score: "))
+    score = parameters(score)
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "G":
+            score = float(input("Score: "))
+            score = parameters(score)
+        elif choice == "P":
+            score_ranks(score)
+        elif choice == "S":
+            print_stars(score)
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Farewell")
+
+
+def parameters(score):
+    while score < 0 or score > 100:
+        print("Invalid score")
+        score = float(input("Enter score: "))
+    return score
+
+
+def score_ranks(score):
+    if score >= 90:
+        print("Excellent")
+    elif score >= 50:
+        print("Pass")
+    else:
+        print("Fail")
+
+
+def print_stars(score):
+    print("*" * score)
+
+
+main()
