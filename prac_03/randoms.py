b329cc963b65f5035bc564a02878e85a87c61c5b
JarrodE <jarrod.eaton@my.jcu.edu.au>
2023-10-01 22:30:28 +1000

    Add random numbers practical

---
+++ prac_03/randoms.py
@@ -0,0 +1,10 @@
+import random
+
+# Line 1
+# A random number between 5 and 19.
+# Line 2
+# A random number of 3, 5, 7 or 9. A 4 could not be produced
+# Line 3
+# A random number between 2.500000000000000 and 5.499999999999999.
+
+print(random.randint(5, 101))
