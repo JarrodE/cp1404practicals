2aefd8a174268b8b9aeafaab0f0fa14e60e46d9d
JarrodE <jarrod.eaton@my.jcu.edu.au>
2023-10-09 11:57:16 +1000

    Update calculate income function and refactor Months variable

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -7,18 +7,22 @@ Starter code for cumulative total income program
 def main():
     """Display income report for incomes over a given number of months."""
     incomes = []
-    months = int(input("How many months? "))
+    number_of_months = int(input("How many months? "))
 
-    for month in range(1, months + 1):
-        income = float(input("Enter income for month " + str(month) + ": "))
+    for month in range(1, number_of_months + 1):
+        income = float(input(f"Enter income for month {month}: "))
         incomes.append(income)
 
     print("\nIncome Report\n-------------")
+    calculate_income(incomes, number_of_months)
+
+
+def calculate_income(incomes, number_of_months):
     total = 0
-    for month in range(1, months + 1):
+    for month in range(1, number_of_months + 1):
         income = incomes[month - 1]
         total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+        print(f"Month {month:2} - Income: ${income:5.2f} Total: ${total:5.2f}")
 
 
 main()
