0fab35f0f79dd2ac28257296121dcad272cc7e89
JarrodE <jarrod.eaton@my.jcu.edu.au>
2023-10-31 17:59:49 +1000

    Update guitars prac to get user input and to display guitars

---
+++ prac_06/guitars.py
@@ -0,0 +1,35 @@
+"""CP1404 Practical - Guitars
+Estimated: 30 minutes
+Actual:  minutes
+Current time: 17:50
+Finish time:
+"""
+from guitar import Guitar
+
+
+def main():
+    """Main program to store and display guitar details."""
+    guitars = []
+    print("My guitars!")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        new_guitar = Guitar(name, year, cost)
+        guitars.append(new_guitar)
+        print(f"{new_guitar} added.")
+        name = input("Name: ")
+
+    # For testing
+    # guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    # guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+    # Display all the stored guitars
+    print("These are my guitars:")
+    for i, guitar in enumerate(guitars, 1):
+        vintage_string = " (vintage)" if guitar.is_vintage() else ""
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+
+
+if __name__ == "__main__":
+    main()
