73398b92a56d82b8c7af1f705243e10a786e948f
JarrodE <jarrod.eaton@my.jcu.edu.au>
2023-10-31 17:22:10 +1000

    Add guitar class attributes

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -5,3 +5,9 @@ Current time: 17:20
 Finish time:
 """
 
+
+class Guitar:
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
