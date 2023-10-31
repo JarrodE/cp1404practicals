19ff9ed75543c6ecbb5e8fab4c9d5668656f9be6
JarrodE <jarrod.eaton@my.jcu.edu.au>
2023-10-31 17:17:22 +1000

    Update time to finish exercise

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -1,7 +1,8 @@
 """CP1404 Practical - Languages
 Estimated: 30 minutes
+Actual: 21 minutes
 Current time: 16:54
-Finish time:
+Finish time: 17:15
 """
 
 from programming_language import ProgrammingLanguage
--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,7 +1,8 @@
 """CP1404 Practical - Programming Language
 Estimated: 30 minutes
+Actual: 21 minutes
 Current time: 16:54
-Finish time:
+Finish time: 17:15
 """
 
 
