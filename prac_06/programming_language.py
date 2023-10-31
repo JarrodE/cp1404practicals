eb0b9d45c6ea287ca9c24dc73af4d3cc70f35eca
JarrodE <jarrod.eaton@my.jcu.edu.au>
2023-10-31 17:09:50 +1000

    Add __init__ function

---
+++ prac_06/programming_language.py
@@ -0,0 +1,18 @@
+"""CP1404 Practical - Programming Language
+Estimated: 30 minutes
+Current time: 16:54
+Finish time:
+"""
+
+
+class ProgrammingLanguage:
+    """Represent a Programming Language object."""
+
+    def __init__(self, name, typing, reflection, year):
+        """ Initialise ProgrammingLanguage instance."""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+
