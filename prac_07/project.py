19ec1ddcdbfdac338e192d3df0dc223f0147375a
JarrodE <jarrod.eaton@my.jcu.edu.au>
2023-11-05 17:34:00 +1000

    Update project class initialise method

--- prac_07/project.py
+++ prac_07/project.py
@@ -6,24 +6,27 @@ import datetime
 
 
 class Project:
-    """Represent a project object."""
+    """
+    Represents a project with attributes for name, start date, priority, cost estimate, and completion percentage.
+    """
 
-    def __int__(self, name, start_date, priority, cost_estimate, completion_percentage):
-        """Initialise a project object."""
+    def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
+        """
+        Initialise a new Project instance.
+        """
         self.name = name
-        self.start_date = datetime.datetime.strptime(start_date, "%d/%m/%Y").date()
-        self.priority = int(priority)
-        self.cost_estimate = float(cost_estimate)
-        self.completion_percentage = int(completion_percentage)
-
-    def __str__(self):
-        """Return a string representation of the Project object."""
-        return (f"{self.name}, start: {self.start_date.strftime('%d/%m/%Y')}, priority {self.priority},"
-                f"estimate: ${self.cost_estimate:.2f}, completion: {self.completion_percentage}%")
+        self.start_date = start_date
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.completion_percentage = completion_percentage
 
     def __lt__(self, other):
-        """Compare two projects based on their priority."""
+        """Return True if this project's priority is less than another project's priority, False otherwise."""
         return self.priority < other.priority
+
+    def __str__(self):
+        """Return a string representation of the project."""
+        return f"{self.name}, start: {self.start_date.strftime('%d/%m/%Y')}, priority {self.priority}, estimate: ${self.cost_estimate:.2f}, completion: {self.completion_percentage}%"
 
     def is_complete(self):
         """Check if the project is complete"""
