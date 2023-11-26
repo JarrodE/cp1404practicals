ebac6e59eb445de909d1efc18454b4bcf64bfca3
JarrodE <jarrod.eaton@my.jcu.edu.au>
2023-11-26 22:12:20 +1000

    Update testing program to prac 10

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -9,7 +9,7 @@ from prac_06.car import Car
 
 def repeat_string(s, n):
     """Repeat string s, n times, with spaces in between."""
-    return s * n
+    return " ".join([s] * n)
 
 
 def is_long_word(word, length=5):
@@ -22,7 +22,7 @@ def is_long_word(word, length=5):
     >>> is_long_word("Python", 6)
     True
     """
-    return len(word) > length
+    return len(word) >= length
 
 
 def run_tests():
@@ -39,20 +39,23 @@ def run_tests():
     # used to see if Car's init method sets the odometer correctly
     # this should pass (no output)
     test_car = Car()
-    assert test_car._odometer == 0, "Car does not set odometer correctly"
+    assert test_car.odometer == 0, "Car does not set odometer correctly"
+    assert default_car.fuel == 0, "Car does not set default fuel correctly"
 
     # TODO: 2. write assert statements to show if Car sets the fuel correctly
     # Note that Car's __init__ function sets the fuel in one of two ways:
     # using the value passed in or the default
     # You should test both of these
     test_car = Car(fuel=10)
+    assert test_car.fuel == 10, "Car does not set specified fuel correctly"
 
 
 run_tests()
 
 # TODO: 3. Uncomment the following line and run the doctests
 # (PyCharm may see your >>> doctest comments and run doctests anyway.)
-# doctest.testmod()
+doctest.testmod()
+
 
 # TODO: 4. Fix the failing is_long_word function
 # (don't change the tests, change the function!)
@@ -66,3 +69,14 @@ run_tests()
 # and one more you decide (one that is valid!)
 # test this and watch the tests fail
 # then write the body of the function so that the tests pass
+def format_as_sentence(phrase):
+    """
+    Formats a given phrase as a sentence.
+    >>> format_as_sentence('hello')
+    'Hello.'
+    >>> format_as_sentence('It is an ex parrot.')
+    'It is an ex parrot.'
+    >>> format_as_sentence('this is a test')
+    'This is a test.'
+    """
+    pass
